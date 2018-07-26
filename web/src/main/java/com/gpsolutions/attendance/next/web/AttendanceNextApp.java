58eeb90043c2c3aa395e250e718fb1feab04ff37
p-prakapienka <prakapienka.work@gmail.com>
2018-07-26 17:58:51 +0300

    0.0.1 security

--- web/src/main/java/com/gpsolutions/attendance/next/web/AttendanceNextApp.java
+++ web/src/main/java/com/gpsolutions/attendance/next/web/AttendanceNextApp.java
@@ -1,10 +1,13 @@
 package com.gpsolutions.attendance.next.web;
 
+import com.gpsolutions.ldap.config.LdapSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(LdapSecurityConfiguration.class)
 public class AttendanceNextApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
