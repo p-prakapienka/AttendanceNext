0f2c742eb6ceff774c17cd3a40b031d4f321eeaf
p-prakapienka <prakapienka.work@gmail.com>
2018-07-26 14:18:04 +0300

    0.0.1 index.html

--- web/src/main/java/com/gpsolutions/attendance/next/web/AttendanceNextApp.java
+++ web/src/main/java/com/gpsolutions/attendance/next/web/AttendanceNextApp.java
@@ -2,16 +2,10 @@ package com.gpsolutions.attendance.next.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class AttendanceNextApp extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AttendanceNextApp.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(AttendanceNextApp.class, args);
