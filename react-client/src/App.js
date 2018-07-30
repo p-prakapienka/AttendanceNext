96b74a8a58be3370f544d1bc28639b218eeb092a
p-prakapienka <prakapienka.work@gmail.com>
2018-07-30 20:34:19 +0300

    0.0.1 rest call and style fixes

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+
+        this.state = {
+            dayReport: {}
+        };
+
+        this.fetchDayReport = this.fetchDayReport.bind(this);
+    }
+
+    fetchDayReport() {
+        fetch('/api/report/day', {
+            credentials: "same-origin"
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            this.setState({dayReport : data})
+        });
+    }
+
+    componentDidMount() {
+        this.fetchDayReport();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Attendance Next</h1>
+                </header>
+                <p className="App-intro">
+                    Today: {this.state.dayReport.total}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
